621e36c4697fa8e94449be79d82f92203d16d10f
Yanick Schraner <yanick.schraner@bs.ch>
2024-09-09 10:50:38 +0200

    Improve keyword extractoin promts

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import streamlit as st
 import pandas as pd
 from dotenv import load_dotenv
-from mrvn import extract_keywords_marvin
-from llamaindex import extract_keywords_llama_index
-from keyllm import extract_keywords_keyllm
+load_dotenv()
+
+from extract_keywords import extract_keywords, extract_few_shot_keywords, extract_keywords_cot
 import json
+
+
 
 def show_pdf(file_url):
     pdf_js = f"""
@@ -12,7 +14,7 @@ def show_pdf(file_url):
     """
     st.markdown(pdf_js, unsafe_allow_html=True)
 
-load_dotenv()
+
 st.set_page_config(layout="wide")
 st.title("Keyword Extractor")
 
@@ -45,23 +47,26 @@ if run_extraction:
             url = data["url"]
             details_text = data["content"]
 
-            # keyllm_keywords = extract_keywords_keyllm(details_text)
-            # marvin_keywords = extract_keywords_marvin(details_text)
-            llama_index_keywords = extract_keywords_llama_index(details_text)
+            message, keywords = extract_keywords(details_text)
+            message_few_shot, keywords_few_shot = extract_few_shot_keywords(details_text)
+            message_cot, keywords_cot = extract_keywords_cot(details_text)
 
             result_col1, result_col2 = st.columns(2)
             
             with result_col1:
                 st.write(url)
 
-                # st.subheader("Extracted Keywords KeyLLM")
-                # st.write(", ".join(keyllm_keywords))
-
-                # st.subheader("Extracted Keywords Marvin")
-                # st.write(", ".join(marvin_keywords))
-
                 st.subheader("Extracted Keywords")
-                st.write(", ".join(llama_index_keywords))
+                st.write(", ".join(keywords))
+
+                st.subheader("Extracted Keywords Few Shot")
+                st.write(", ".join(keywords_few_shot))
+
+                st.subheader("Extracted Keywords COT")
+                st.write(", ".join(keywords_cot))
+
+                st.subheader("Thought process: ")
+                st.write(message_cot)
                 
                 st.subheader("Original Text")
                 st.write(details_text)
---
+++ extract_keywords.py
@@ -0,0 +1,129 @@
+import os
+from openai import OpenAI
+import httpx
+
+MAX_TOKENS = 4096
+
+proxy_url = os.environ.get("PROXY_URL")
+model = "gpt-4o"  # "gpt-4o-mini"
+llm = OpenAI(
+    api_key=os.getenv("OPENAI_API_KEY"),
+    http_client=httpx.Client(proxy=proxy_url),
+)
+
+SYSTEM_PROMPT = """You are a SEO expert and your goal is to improve the meta data for the given document. \
+                Come up with at most 20 unique keywords for this document. \
+                The keywords should match with search queries of potential users that are trying to find this document. \
+                Do not copy terms from the document. \
+                For each proposed keyword include synonyms using simple A2 level language. \
+                Include the synonyms directly in your output list. \
+                Do not include numbers, years and Basel-Stadt in your keyword proposal. \
+                Only German keywords. \
+                Format keywords as comma separated list at the end of your answer after Keywords: """
+
+
+SYSTEM_PROMPT_FEW_SHOT = """You are a SEO expert and your goal is to improve the meta data for the given document. \
+                Come up with at most 20 unique keywords for this document. \
+                The keywords should match with search queries of potential users that are trying to find this document. \
+                Do not copy terms from the document. \
+                For each proposed keyword include synonyms using simple A2 level language. \
+                Include the synonyms directly in your output list. \
+                Do not include numbers, years and Basel-Stadt in your keyword proposal. \
+                Only German keywords. \
+                Format keywords as comma separated list at the end of your answer after 'Keywords:' \
+                Document: '''Teuerungsrechner 05 Preise Basler Index der Konsumentenpreise Mit dieser Anwendung können Sie mit dem Basler Index indexierte Beträge (z.B. Alimentenzahlungen, Renten, usw.) oder die Teuerungsrate für einen beliebigen Zeitraum berechnen. Sie haben die Möglichkeit, die Indexreihen mit jahresdurchschnittlichen (ab 1915) oder monatlichen (ab 1940) Werten für die Berechnung zu verwenden.'''  \
+                Keywords: Inflation berechnen, Preisindex Schweiz, Konsumentenpreisindex, Teuerungsrate Schweiz, Lebenshaltungskosten Schweiz, Indexierte Beträge berechnen, Inflationsrechner, Preisentwicklung Schweiz, Preisveränderung Schweiz, Teuerung Schweiz, Kostensteigerung Schweiz, Preissteigerung, Inflationsrate, Preisvergleich, Konsumentenpreise, Preisstatistik, Preisänderung, Kosten, Lohnentwicklung \
+                Document: '''Grenzwertüberschreitungen Ozon \n Anzahl Überschreitungen des Stunden-Grenzwertes pro Jahr, Region Basel \n Im Jahr 2023 lagen in Basel-Stadt 304 Stunden-Mittelwerte über dem Grenzwert für Ozon. Laut Luftreinhalte-Verordnung dürfte der Grenzwert höchstens einmal pro Jahr überschritten werden. \n Anzahl Stunden-Mittelwerte pro Jahr, welche über dem in der Luftreinhalte-Verordnung (LRV) gesetzlich festgelegten Grenzwert von 120 μg/m<sup>3</sup> liegen, unterschieden nach Art der Lage der Messstationen: Ländliche Höhenlagen (Chrischona, Brunnersberg, Chaumont (NABEL)), Agglomeration (Binningen (NABEL), Dornach (SO)), Stadt Basel (Basel St.Johanns-Platz).<br>Ozon ist ein sekundärer Luftschadstoff, es wird bei intensiver Sonneneinstrahlung aus den Vorläuferschadstoffen Stickstoffoxiden und flüchtigen organischen Verbindungen in Kombination mit Sauerstoff gebildet. In städtischen Gebieten ist die Ozonkonzentration in der Regel tiefer als in ländlichen, da Stickstoffmonoxid aus Autoabgasen mit Ozon reagiert und dieses so abgebaut wird.''' \
+                Keywords: Ozonbelastung, Ozonwerte, Luftverschmutzung, Luftqualität, Luftreinhalte-Verordnung, Grenzwertüberschreitung, Luftschadstoffe, städtische Luftqualität, ländliche Luftqualität, Messstationen, Ozonbildung, Stickstoffoxide, Abgas, Autoabgase, Luftüberwachung, Umweltverschmutzung, Luftreinheit, Schadstoffmessung, Luftschutz, Verschmutzung Vororte \
+                """
+
+SYSTEM_PROMPT_COT = """You are a SEO expert and your goal is to improve the meta data for the given document. \
+                Come up with at most 20 unique keywords for this document. \
+                The keywords should match with search queries of potential users that are trying to find this document. \
+                Do not copy terms from the document. \
+                For each proposed keyword include synonyms using simple A2 level language. \
+                Include the synonyms directly in your output list. \
+                Do not include numbers, years and Basel-Stadt in your keyword proposal. \
+                Only German keywords. \
+                Format keywords as comma separated list at the end of your answer after Keywords: \
+                Let's think step by step. """
+
+KEYWORD_EXTRACT_TEMPLATE = """\
+<document> {context_str} </document>. \
+You are a SEO expert and your goal is to improve the meta data for the given document. \
+Come up with at most {keywords} unique keywords for this document. \
+The keywords should match with search queries of potential users that are trying to find this document. \
+Do not copy terms from the document. \
+For each keyword propose synonyms using simple A2 language. \
+Do not include numbers, years and Basel-Stadt in your keyword proposal. \
+Only German keywords. \
+Format keywords as comma separated at the end of your anser. \
+Let's think step by step. """
+# Keywords: """
+
+def extract_keywords(text: str):
+    response = llm.chat.completions.create(
+        model=model,
+        messages=[
+            {
+            "role": "system",
+            "content": SYSTEM_PROMPT
+            },
+            {
+            "role": "user",
+            "content": f'"""{text}"""'
+            }
+        ],
+        temperature=0.5,
+        max_tokens=MAX_TOKENS,
+        top_p=1
+        )
+    message = response.choices[0].message.content
+    keywords = message.split("Keywords:")[-1]
+    keywords = keywords.split(",")
+    return message, keywords
+
+def extract_few_shot_keywords(text: str):
+    response = llm.chat.completions.create(
+        model=model,
+        messages=[
+            {
+            "role": "system",
+            "content": SYSTEM_PROMPT_FEW_SHOT
+            },
+            {
+            "role": "user",
+            "content": f"""Document: '''{text}''' \
+                Keywords: """
+            }
+        ],
+        temperature=0.5,
+        max_tokens=MAX_TOKENS,
+        top_p=1
+        )
+    message = response.choices[0].message.content
+    keywords = message.split("Keywords:")[-1]
+    keywords = keywords.split(",")
+    return message, keywords
+
+def extract_keywords_cot(text: str):
+    response = llm.chat.completions.create(
+        model=model,
+        messages=[
+            {
+            "role": "system",
+            "content": SYSTEM_PROMPT_COT
+            },
+            {
+            "role": "user",
+            "content": f'Dokument: """{text}"""'
+            }
+        ],
+        temperature=0.5,
+        max_tokens=MAX_TOKENS,
+        top_p=1
+        )
+    message = response.choices[0].message.content
+    keywords = message.split("Keywords:")[-1]
+    keywords = keywords.split(",")
+    return message, keywords
